803a08504998e903022edff92f8bfed9f5679640
neoparkk <neopark389@gmail.com>
2024-03-30 15:54:11 -0400

    Project files

---
+++ Assets/Scripts/Movement/Slide.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slide : MonoBehaviour
+{
+    Rigidbody rig;
+    CapsuleCollider collider;
+
+    float originalHeight;
+    public float reducedHeight;
+
+    public float slideSpeed = 10f;
+
+    bool isSliding;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        collider = GetComponent<CapsuleCollider>();
+        rig = GetComponent<Rigidbody>();
+        originalHeight = collider.height;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftControl) && Input.GetKey(KeyCode.W))
+            Sliding();
+        else if (Input.GetKeyUp(KeyCode.LeftControl))
+            GoUp();
+    }
+
+    private void Sliding()
+    {
+        collider.height = reducedHeight;
+        rig.AddForce(transform.forward * slideSpeed, ForceMode.VelocityChange);
+    }
+    private void GoUp()
+    {
+        collider.height=originalHeight;
+    }
+}
